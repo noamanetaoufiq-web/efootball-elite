0e7265d2f1ca16dba0d53c611de4b9c0280bc978
Noamane <noamanetaoufiq@gmail.com>
2026-04-22 20:49:46 +0100

    feat: add Celtic and more real club logos

--- generateLogos.js
+++ generateLogos.js
@@ -29,16 +29,39 @@ const generateLogos = () => {
         { name: "Liverpool", logo: "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg" },
         { name: "Arsenal", logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg" },
         { name: "Chelsea", logo: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg" },
+        { name: "Tottenham Hotspur", logo: "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg" },
+        { name: "Aston Villa", logo: "https://upload.wikimedia.org/wikipedia/en/f/f9/Aston_Villa_FC_crest_%282016%29.svg" },
         { name: "Bayern Munich", logo: "https://upload.wikimedia.org/wikipedia/commons/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg" },
         { name: "Borussia Dortmund", logo: "https://upload.wikimedia.org/wikipedia/commons/6/67/Borussia_Dortmund_logo.svg" },
+        { name: "Bayer Leverkusen", logo: "https://upload.wikimedia.org/wikipedia/en/5/59/Bayer_04_Leverkusen_logo.svg" },
         { name: "Paris Saint-Germain", logo: "https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg" },
+        { name: "Olympique Lyonnais", logo: "https://upload.wikimedia.org/wikipedia/fr/e/e2/Olympique_lyonnais_%28logo%29.svg" },
+        { name: "Olympique de Marseille", logo: "https://upload.wikimedia.org/wikipedia/fr/4/43/Logo_Olympique_de_Marseille.svg" },
         { name: "Juventus", logo: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Juventus_FC_2017_icon_%28black%29.svg" },
         { name: "AC Milan", logo: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg" },
-        { name: "Inter Milan", logo: "https://upload.wikimedia.org/wikipedia/commons/0/05/FC_Internazionale_Milano_2021.svg" }
+        { name: "Inter Milan", logo: "https://upload.wikimedia.org/wikipedia/commons/0/05/FC_Internazionale_Milano_2021.svg" },
+        { name: "Napoli", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2d/SSC_Napoli_logo.svg" },
+        { name: "Atletico Madrid", logo: "https://upload.wikimedia.org/wikipedia/en/f/f4/Atletico_Madrid_2017_logo.svg" },
+        { name: "Benfica", logo: "https://upload.wikimedia.org/wikipedia/en/a/a2/SL_Benfica_logo.svg" },
+        { name: "FC Porto", logo: "https://upload.wikimedia.org/wikipedia/en/f/f1/FC_Porto.svg" },
+        { name: "Ajax", logo: "https://upload.wikimedia.org/wikipedia/en/7/79/Ajax_Amsterdam.svg" },
+        { name: "Celtic FC", logo: "https://upload.wikimedia.org/wikipedia/en/3/3a/Celtic_FC_crest.svg" }
     ];
 
     europeTeams.forEach(t => {
         logos.push({ id: idCounter++, name: t.name, logo: t.logo, region: "Europe" });
+    });
+
+    // World Teams (Saudi, MLS, etc)
+    const worldTeams = [
+        { name: "Al Nassr", logo: "https://upload.wikimedia.org/wikipedia/en/2/2b/Al_Nassr_FC_logo.svg", region: "Asia" },
+        { name: "Al Hilal", logo: "https://upload.wikimedia.org/wikipedia/en/f/fa/Al-Hilal_Saudi_FC_logo.svg", region: "Asia" },
+        { name: "Al Ittihad", logo: "https://upload.wikimedia.org/wikipedia/en/b/bd/Al-Ittihad_Saudi_Club_logo.svg", region: "Asia" },
+        { name: "Inter Miami", logo: "https://upload.wikimedia.org/wikipedia/en/5/5c/Inter_Miami_CF_logo.svg", region: "Americas" }
+    ];
+
+    worldTeams.forEach(t => {
+        logos.push({ id: idCounter++, name: t.name, logo: t.logo, region: t.region });
     });
 
     // Generate remaining to hit 300+
