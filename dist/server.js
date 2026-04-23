90beae1699747a5d03996889889494785bb19876
Noamane <noamanetaoufiq@gmail.com>
2026-04-23 15:18:41 +0100

    fix: resolve Express 5 path-to-regexp error for catch-all route

--- dist/server.js
+++ dist/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Catch-all for SPA
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
